0b821327634765948d8453c30f49c0691e537e86
Mike <mikewycklendt@gmail.com>
2020-04-19 11:57:13 -0500

    postgres IP

--- demo1/flask-hello-app.py
+++ demo1/flask-hello-app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:F00tBall@localhost:5432/example'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:F00tBall@127.0.0.1:5432/example'
 db = SQLAlchemy(app)
 
 class Person(db.Model):
