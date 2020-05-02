db3d0751446208fba5a4ed30e95148088b776f37
Mike <mikewycklendt@gmail.com>
2020-05-02 13:49:53 -0500

    js formhandler

--- demo1/flask-hello-app.wsgi
+++ demo1/flask-hello-app.wsgi
@@ -4,4 +4,3 @@ import sys
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, '/var/www/html/demo1/')
 from application import app as application
-application.secret_key = 'super_secret_key'
--- todoapp/app.py
+++ todoapp/app.py
@@ -1,12 +1,74 @@
-from flask import Flask, render_template
+#!/usr/bin/python3
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+import sys
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@3.134.26.61:5432/todoapp'
+db = SQLAlchemy(app)
+#db = SQLAlchemy(app, session_options={"expire_on_commit": False})
+
+class Todo(db.Model):
+	__tablename__ = 'todos'
+	id = db.Column(db.Integer, primary_key=True)
+	description = db.Column(db.String(), nullable=False)
+
+	def __repr__(self):
+		return f'<Todo {self.id} {self.description}>'	
+
+db.create_all()
+
+todo1 = Todo(description='Todo Thing 1')
+db.session.add(todo1)
+
+todo2 = Todo(description='Todo Thing 2')
+db.session.add(todo2)
+
+todo3 = Todo(description='Todo Thing 3')
+db.session.add(todo3)
+
+todo4 = Todo(description='Todo Thing 4')
+db.session.add(todo4)
+
+todo5 = Todo(description='Todo Thing 5')
+db.session.add(todo5)
+
+db.session.commit()
+
+
 
 @app.route('/')
 def index():
-	return render_template('index.html', data=[
-		{'description: "Todo 1'},
-		{'description: "Todo 2'},
-		{'description: "Todo 3'}
-	])
+	return render_template('index.html', data=Todo.query.all())
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+	error = False
+	body = {}
+#	description = request.form.get('description', '')
+#	return render_template('index.html')
+	try:
+		description = request.get_json()['description']
+		todo = Todo(description=description)
+		body['description'] = todo.description
+		db.session.add(todo)
+		db.session.commit()
+	except:
+		error=True
+		db.session.rollback()
+		print(sys.exec_info())
+	finally:
+		db.session.close()
+	if error:
+		abort (400)
+	else:
+		return jsonify(body)
+#		return jsonify({
+#			'description': todo.description
+#		})
+
+#	return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
