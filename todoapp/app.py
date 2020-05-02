15755f5971c60b4affd68a8068cdcecd66f69598
Mike <mikewycklendt@gmail.com>
2020-05-02 13:54:31 -0500

    commented out bad code

--- todoapp/app.py
+++ todoapp/app.py
@@ -14,8 +14,8 @@ class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.String(), nullable=False)
 
-	def __repr__(self):
-		return f'<Todo {self.id} {self.description}>'	
+#	def __repr__(self):
+#		return f'<Todo {self.id} {self.description}>'	
 
 db.create_all()
 
