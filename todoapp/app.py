106ea39e09678961243a04642602d4ba9cdc4688
Mike <mikewycklendt@gmail.com>
2020-05-06 11:17:59 -0500

    update delete

--- todoapp/app.py
+++ todoapp/app.py
@@ -29,7 +29,7 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
-	return render_template('index.html', data=Todo.query.all())
+	return render_template('index.html', data=Todo.query.order_by('id').all())
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -58,6 +58,31 @@ def create_todo():
 #		})
 
 #	return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>/set-completed', methoda=['POST'])
+def set_completed_todo(todo_id):
+	try:
+		completed = request.get_json()['completed']
+		print('completed', completed)
+		todo = Todo.query.get(todo_id)
+		todo.completed = completed
+		db.session.commit()
+	except:
+		db.session.rollback()
+	finally:
+		db.session.close
+	return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>', methoda=['DELETE'])
+def delete_todo(todo_id):
+	try:
+		Todo.query.filter_by(id=todo_id).delete()
+		db.session.commit()
+	except:
+		db.session.rollback()
+	finally:
+		db.session.close()
+	return jsonify({'success': True })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
