8d81ea1b62edb19c31560d48e7276a02690f858a
Mike <mikewycklendt@gmail.com>
2020-05-09 13:06:39 -0500

    lists table

--- todoapp/app.py
+++ todoapp/app.py
@@ -17,6 +17,13 @@ class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.String(), nullable=False)
 	completed = db.Column(db.Boolean, nullable=False, default=False)
+	list_id = db.Column(db.Integer, db.ForeignKey ('todolists.id'), nullable==False)
+
+class TodoList(db.Model):
+	__tablename__ = 'todolists'
+	id = dbColumn(db.Integer, primary_key=True)
+	name = db.Column(dbString(), nullable=False)
+	todos = db.relationship('Todo', backref='list', lazy=True)
 
 #	def __repr__(self):
 #		return f'<Todo {self.id} {self.description}>'	
