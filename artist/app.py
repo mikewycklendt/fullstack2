e2111d62625eca635264f910e14770c52ad7eada
Mike <mikewycklendt@gmail.com>
2020-05-19 08:24:23 -0500

    db update

--- artist/app.py
+++ artist/app.py
@@ -42,9 +42,12 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    seeking = db.Column(db.Boolean, nullable=False)
-    seekingDesc = db.Column(db.String())
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String())
     genres = db.Column(db.String())
+    website = db.Column(db.String())
+    past_shows = db.relationship('Show', backref='shows', lazy=True)
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -58,6 +61,17 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String())
+    website = db.Column(db.String())
+    past_shows = db.relationship('Show', backref='shows', lazy=True)
+
+class Show(db.Model):
+    __tablename__ = 'show'
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey(venue.id), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey(artist.id), nullable=False)
+    start_time = db.Column(db.DateTime(), nullable=False)
 
 #db.create_all()
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
