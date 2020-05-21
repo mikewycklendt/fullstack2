d3def1b6a6255bd724d9a64b69108d7d411aee81
Mike <mikewycklendt@gmail.com>
2020-05-21 10:17:11 -0500

    db.array

--- artist/app.py
+++ artist/app.py
@@ -44,7 +44,7 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String())
-    genres = db.Column(db.String())
+    genres = db.Column(db.ARRAY(db.String())
     website = db.Column(db.String())
     pastshows = db.relationship('Show', backref='pastshows', lazy=True)
 
@@ -58,7 +58,7 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
@@ -250,22 +250,22 @@ def create_venue_submission():
   facebook = request.form['facebook_link']
   seeking = request.form['seeking_talent']
   seekingDesc = request.form['seeking_description']
-  genres = request.form['genres']
+  genres = request.form.getlist('genres')
   website = request.form['website']
-
+  print(genres)
   if seeking == 'True':
     seeking = True
   else:
     seeking = False
   #try:
-  newEntry = Venue(name=name, city=city, state=venueState, address=address, phone=phone, image_link=image, facebook_link=facebook, seeking_talent=seeking, seeking_description=seekingDesc, genres=genres, website=website)
-  db.session.add(newEntry)
-  db.session.commit()
+  #newEntry = Venue(name=name, city=city, state=venueState, address=address, phone=phone, image_link=image, facebook_link=facebook, seeking_talent=seeking, seeking_description=seekingDesc, genres=genres, website=website)
+  #db.session.add(newEntry)
+  #db.session.commit()
   #except Exception as e:
   #  print(e)
   #  db.session.rollback()
   #finally:
-  db.session.close()
+  #db.session.close()
   #if error:
   #  flash('An error occured. Venue ' + name + ' could not be listed.')
   #  return redirect(url_for('index'))
