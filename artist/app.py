d34b0fd8fcde365997592e65b373c0c3063de9c8
Mike <mikewycklendt@gmail.com>
2020-05-23 10:01:23 -0500

    typo

--- artist/app.py
+++ artist/app.py
@@ -266,9 +266,8 @@ def show_artist(artist_id):
   shows = Show.query.filter_by(artist_id=artist_id).all()
   for show in shows:
     print(show.id)
-
-  data = [
-    {"id": 3,
+  data={
+    "id": 3,
     "name": "The Wild Sax Band",
     "genres": ["Jazz", "Classical"],
     "city": "San Francisco",
@@ -296,6 +295,7 @@ def show_artist(artist_id):
     "past_shows_count": 0,
     "upcoming_shows_count": 3,
   }
+  data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
   return render_template('pages/show_artist.html', artist=data)
 
 #  Update
