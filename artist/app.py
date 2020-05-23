0bc93eaa355cf71ecf49ea9169dd0a1dd8c450ff
Mike <mikewycklendt@gmail.com>
2020-05-23 09:09:19 -0500

    artist.shows

--- artist/app.py
+++ artist/app.py
@@ -263,7 +263,7 @@ def show_artist(artist_id):
   # shows the venue page with the given venue_id
   # TODO: replace with real venue data from the venues table, using venue_id
   artist = db.session.query(Artist).filter_by(id=artist_id)
-  print(artist.past_shows)
+  print(artist.shows)
   data1={
     "id": 4,
     "name": "Guns N Petals",
