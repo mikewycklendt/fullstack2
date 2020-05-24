e4a47c643d2f552e8cee8c4be240e6bb6f0b65ca
Mike <mikewycklendt@gmail.com>
2020-05-24 08:26:22 -0500

    commit

--- artist/app.py
+++ artist/app.py
@@ -15,6 +15,7 @@ from forms import *
 from flask_migrate import Migrate
 import config
 from forms import VenueForm, ShowForm, ArtistForm
+import db
 
 app = Flask(__name__)
 moment = Moment(app)
@@ -23,49 +24,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
-
-class Venue(db.Model):
-    __tablename__ = 'venue'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String())
-    genres = db.Column(db.ARRAY(db.String))
-    website = db.Column(db.String())
-    pastshows = db.relationship('Show', backref='pastshows', lazy=True)
-
-class Artist(db.Model):
-    __tablename__ = 'artist'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
-    seeking_description = db.Column(db.String())
-    website = db.Column(db.String())
-    past_shows = db.relationship('Show', backref='shows', lazy=True)
-
-class Show(db.Model):
-    __tablename__ = 'show'
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-    artist_name = db.Column(db.String())
-    venue_name = db.Column(db.String())
-    venue_image = db.Column(db.String())
-    start_time = db.Column(db.DateTime(), nullable=False)
-
-
 
 def format_datetime(value, format='medium'):
   date = dateutil.parser.parse(value)
