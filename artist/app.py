f6a5d3f6e523edb7044910fc3dade11fc1bca4b3
Mike <mikewycklendt@gmail.com>
2020-05-24 09:04:57 -0500

    db in app

--- artist/app.py
+++ artist/app.py
@@ -23,7 +23,48 @@ moment = Moment(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@3.134.26.61:5432/artistapp'
 db = SQLAlchemy(app)
 
-#migrate = Migrate(app, db)
+migrate = Migrate(app, db)
+
+class Venue(db.Model):
+    __tablename__ = 'venue'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String())
+    genres = db.Column(db.ARRAY(db.String))
+    website = db.Column(db.String())
+    pastshows = db.relationship('Show', backref='pastshows', lazy=True)
+
+class Artist(db.Model):
+    __tablename__ = 'artist'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String())
+    website = db.Column(db.String())
+    past_shows = db.relationship('Show', backref='shows', lazy=True)
+
+class Show(db.Model):
+    __tablename__ = 'show'
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+    artist_name = db.Column(db.String())
+    venue_name = db.Column(db.String())
+    venue_image = db.Column(db.String())
+    start_time = db.Column(db.DateTime(), nullable=False)
 
 def format_datetime(value, format='medium'):
   date = dateutil.parser.parse(value)
