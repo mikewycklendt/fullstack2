23ca572bd49d4943c57a1a9ef077d6ea9d9210b5
Mike <mikewycklendt@gmail.com>
2020-05-26 10:05:35 -0500

    artists from db

--- artist/app.py
+++ artist/app.py
@@ -230,16 +230,7 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data = Artist.query.order_by('name').all()
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
@@ -330,10 +321,10 @@ def edit_artist_submission(artist_id):
   finally:
     db.session.close()
   if error:
-    flash('An error occured.  Artist ' + name + ' could not be listed.')
+    flash('An error occured.  Artist ' + name + ' could not be edited.')
     return redirect(url_for('show_artist', artist_id=artist.id))
   else:
-    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    flash('Artist ' + request.form['name'] + ' was successfully edited!')
     return redirect(url_for('show_artist', artist_id=artist.id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
