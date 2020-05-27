2466e882b74679031b2b5b2992f4e85f2bd30da0
Mike <mikewycklendt@gmail.com>
2020-05-27 06:46:49 -0500

    join query

--- artist/app.py
+++ artist/app.py
@@ -54,7 +54,7 @@ def venues():
                             Venue.state, 
                             Venue.name, 
                             Venue.id, 
-                            Show.start_time).join(Show).group_by(Venue.city)
+                            Show.start_time).join(Show).group_by(Venue.city, Venue.state)
   for row in query:
     print(row)
   
