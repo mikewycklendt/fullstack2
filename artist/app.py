dcd82aa3e53e6c59f04288185021ec6713d1fb83
Mike <mikewycklendt@gmail.com>
2020-05-29 09:47:02 -0500

    artist search

--- artist/app.py
+++ artist/app.py
@@ -239,7 +239,7 @@ def search_artists():
 
   search_term=request.form['search_term']
   search = '%' + search_term + '%'
-  data = db.session.query(Artist).filter(Artist.name.like(search)).all()
+  data = db.session.query(Artist).filter(Artist.name.ilike(search)).all()
   for result in data:
     print(result.name)
 
