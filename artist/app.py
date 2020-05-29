2d3330cc97374ec26f0a742c8726a5a463887d11
Mike <mikewycklendt@gmail.com>
2020-05-29 09:22:03 -0500

    search button

--- artist/app.py
+++ artist/app.py
@@ -231,7 +231,7 @@ def artists():
   data = Artist.query.order_by('name').all()
   return render_template('pages/artists.html', artists=data)
 
-@app.route('/artists/search', methods=['POST'])
+@app.route('/artists/search', methods=['POST','GET'])
 def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
@@ -385,47 +385,10 @@ def shows():
 
   print(data)
 
-  #data=[{
-  #  "venue_id": 1,
-  #  "venue_name": "The Musical Hop",
-  #  "artist_id": 4,
-  #  "artist_name": "Guns N Petals",
-  #  "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-  #  "start_time": "2019-05-21T21:30:00.000Z"
-  #}, {
-  #  "venue_id": 3,
-  #  "venue_name": "Park Square Live Music & Coffee",
-  #  "artist_id": 5,
-  #  "artist_name": "Matt Quevedo",
-  #  "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-  #  "start_time": "2019-06-15T23:00:00.000Z"
-  #}, {
-  #  "venue_id": 3,
-  #  "venue_name": "Park Square Live Music & Coffee",
-  #  "artist_id": 6,
-  #  "artist_name": "The Wild Sax Band",
-  #  "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #  "start_time": "2035-04-01T20:00:00.000Z"
-  #}, {
-  #  "venue_id": 3,
-  #  "venue_name": "Park Square Live Music & Coffee",
-  #  "artist_id": 6,
-  #  "artist_name": "The Wild Sax Band",
-  #  "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #  "start_time": "2035-04-08T20:00:00.000Z"
-  #}, {
-  #  "venue_id": 3,
-  #  "venue_name": "Park Square Live Music & Coffee",
-  #  "artist_id": 6,
-  #  "artist_name": "The Wild Sax Band",
-  #  "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #  "start_time": "2035-04-15T20:00:00.000Z"
-  #}]
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')
 def create_shows():
-  # renders form. do not touch.
   form = ShowForm()
   return render_template('forms/new_show.html', form=form)
 
