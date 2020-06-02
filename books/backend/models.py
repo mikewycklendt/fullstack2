4b58452a3c37c3bd2715787d9f2488eecdcaf12f
Mike <mikewycklendt@gmail.com>
2020-06-02 08:46:48 -0500

    typo

--- books/backend/models.py
+++ books/backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = "bookshelf"
-database_path = "postgres://{}:{}@{}/{}".format('student', 'student','localhost:5432', database_name)
+database_path = "postgresql+psycopg2://postgres:postgres@3.134.26.61:5432/bookshelf"
 
 db = SQLAlchemy()
 
