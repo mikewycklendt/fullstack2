5c131db78a7144bee4a3d4fd1c3c9b2bb48a5003
Mike <mikewycklendt@gmail.com>
2020-06-15 09:29:32 -0500

    trivia endpoints

--- trivia/backend/flaskr/__init__.py
+++ trivia/backend/flaskr/__init__.py
@@ -154,7 +154,7 @@ def quizzes():
   previous_questions = body['previous_questions']
   quiz_category = body['quiz_category']
 
-  questions = Question.query.filter(Question.category == quiz_category)\
+  questions = Question.query.filter(Question.category == quiz_category.id)\
     
   formatted_questions = [question.format() for question in questions]
   filtered_questions = []
