8772257c83d607c3702751acd212cdd9283cf4d9
Mike <mikewycklendt@gmail.com>
2020-06-29 08:03:20 -0500

    auth0

--- auth0/app.py
+++ auth0/app.py
@@ -58,7 +58,7 @@ def get_token_auth_header():
 def verify_decode_jwt(token):
     print(token)
     jsonurl = urlopen('https://dcadventuresonline.us.auth0.com/.well-known/jwks.json')
-    jwks = json.load(jsonurl)
+    jwks = json.loads(jsonurl)
     print(jwks)
     unverified_header = token
     rsa_key = {}
