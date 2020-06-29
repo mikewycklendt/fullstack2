b2986df27cd3c57945eaf60511ac4da0b5069869
Mike <mikewycklendt@gmail.com>
2020-06-29 07:17:53 -0500

    auth0

--- auth0/app.py
+++ auth0/app.py
@@ -58,7 +58,8 @@ def get_token_auth_header():
 def verify_decode_jwt(token):
     print(token)
     jsonurl = urlopen('https://dcadventuresonline.us.auth0.com/.well-known/jwks.json')
-    jwks = jsonurl.read()
+    jwks_raw = jsonurl.read()
+    jwks = a[jwks_raw]
     unverified_header = token
     rsa_key = {}
     #if 'kid' not in unverified_header:
