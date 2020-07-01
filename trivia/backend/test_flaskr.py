c8e2b894981769a4f0b548c02e519fe3905ed515
Mike <mikewycklendt@gmail.com>
2020-07-01 11:12:58 -0500

    coffee code

--- trivia/backend/test_flaskr.py
+++ trivia/backend/test_flaskr.py
@@ -109,7 +109,7 @@ class TriviaTestCase(unittest.TestCase):
 		data = json.loads(res.data)
 
 		self.assertEqual(res.status_code, 405)
-		self.assertEqual(data['success'], True)
+		self.assertEqual(data['success'], False)
 		self.assertEqual(data['message'], 'method not allowed')
 
     def test_get_questions_by_category(self):
