ae5c23fca039b32245c50d1125affe993559b989
Mike <mikewycklendt@gmail.com>
2020-07-01 12:14:00 -0500

    coffee code

--- coffee/backend/src/api.py
+++ coffee/backend/src/api.py
@@ -4,8 +4,8 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
-from .auth.auth import AuthError, requires_auth
+from database import models.db_drop_and_create_all, models.setup_db, models.Drink
+from auth import auth.AuthError, auth.requires_auth
 
 app = Flask(__name__)
 setup_db(app)
