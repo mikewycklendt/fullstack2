d682997ac19484d7dd5d6003934af202c2fd4e86
Mike <mikewycklendt@gmail.com>
2020-07-02 10:28:48 -0500

    coffee code

--- coffee/frontend/src/environments/environment.ts
+++ coffee/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'https://3.134.26.61:81', // the running FLASK api server url
   auth0: {
-    url: 'https://dcadventuresonline.us.auth0.com', // the auth0 domain prefix
-    audience: 'coffe', // the audience set for the auth0 app
+    url: 'dcadventuresonline.us.auth0.com', // the auth0 domain prefix
+    audience: 'https://dcadventuresonline.com', // the audience set for the auth0 app
     clientId: 'p3kGaVETQ4TA3BibSj7UKPeSbS5bZ5I5', // the client id generated for the auth0 app
-    callbackURL: 'https://3.134.26.61:80', // the base url of the running ionic application. 
+    callbackURL: 'https://dcadventuresonline.com/tabs/user-page', // the base url of the running ionic application. 
   }
 };
